3fc7d0215e59061a0e55d85105fb0f527f3fdeaa
Yug2801 <95994453+Yug2801@users.noreply.github.com>
2024-10-22 16:46:05 +0530

    Created collection page with mongo connected

--- app/(dashoard)/layout.tsx
+++ app/(dashoard)/layout.tsx
@@ -1,16 +1,17 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../globals.css";
-
 import { ClerkProvider } from "@clerk/nextjs";
+
 import LeftSideBar from "@/components/layout/LeftSideBar";
 import TopBar from "@/components/layout/TopBar";
+import { ToasterProvider } from "@/lib/ToasterProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: " Admin DashBoard",
-  description: "Admin dashboard to manage data",
+  title: "Ecom DashBoard",
+  description: "Admin dashboard to manage Ecom's data",
 };
 
 export default function RootLayout({
@@ -22,12 +23,13 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-        <div className="flex max-lg:flex-col text-grey-1">
-            <LeftSideBar/>
-            <TopBar/>
-            </div>
-
-            {children}</body>
+          <ToasterProvider />
+          <div className="flex max-lg:flex-col text-grey-1">
+            <LeftSideBar />
+            <TopBar />
+            <div className="flex-1">{children}</div>
+          </div>
+        </body>
       </html>
     </ClerkProvider>
   );
